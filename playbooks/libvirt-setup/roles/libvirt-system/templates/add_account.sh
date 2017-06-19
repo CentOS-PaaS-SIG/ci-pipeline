c815530a4a08abc9daa4e83b52fdff5202081c16
Bill Peck <bpeck@redhat.com>
2017-06-19 14:16:42 -0400

    fix cloud-init templates to populate authorized keys properly.

--- playbooks/libvirt-setup/roles/libvirt-system/templates/add_account.sh
+++ playbooks/libvirt-setup/roles/libvirt-system/templates/add_account.sh
@@ -1,7 +1,7 @@
 useradd -m -p '{{ item.value.admin_passwd }}' -G wheel admin
 mkdir -p /home/admin/.ssh
 cat <<'EOF' >> /home/admin/.ssh/authorized_keys
-ssh-rsa {{ item.value.admin_ssh_rsa }}
+{{ item.value.admin_ssh_rsa }}
 EOF
 chown -R admin:admin /home/admin/.ssh
 chmod -R 0700 /home/admin/.ssh
