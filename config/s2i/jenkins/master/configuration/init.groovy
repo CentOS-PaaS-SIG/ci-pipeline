dfe1e33bf5929970853d5082255cec8732d4bee5
Scott Hebert <shebert@redhat.com>
2017-06-22 11:56:14 -0400

    default time zone for Jenkins instance

--- config/s2i/jenkins/master/configuration/init.groovy
+++ config/s2i/jenkins/master/configuration/init.groovy
@@ -11,9 +11,14 @@ logger.info("Enable Slave -> Master Access Control")
 Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
 Jenkins.instance.save()
 
-logger.info("Setup fedora-fedmsg Messaging Provider")
-FedMsgMessagingProvider fedmsg = new FedMsgMessagingProvider("fedora-fedmsg", "tcp://hub.fedoraproject.org:9940", "tcp://hub.fedoraproject.org:9940", "org.fedoraproject");
-GlobalCIConfiguration.get().addMessageProvider(fedmsg)
-
 logger.info("Setting Time Zone to be EST")
 System.setProperty('org.apache.commons.jelly.tags.fmt.timeZone', 'America/New_York')
+
+logger.info("Setup fedora-fedmsg Messaging Provider")
+FedMsgMessagingProvider fedmsg = new FedMsgMessagingProvider("fedora-fedmsg", "tcp://hub.fedoraproject.org:9940", "tcp://hub.fedoraproject.org:9940", "org.fedoraproject");
+try {
+  GlobalCIConfiguration.get().addMessageProvider(fedmsg)
+} catch (Exception e) {
+  logger.info(e.getMessage())
+}
+
