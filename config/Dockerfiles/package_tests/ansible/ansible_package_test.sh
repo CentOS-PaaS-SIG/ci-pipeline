9c34feaef207fba93c5b1ce16ab2ae41f453ea6f
Johnny Bieren <jbieren@redhat.com>
2017-08-03 13:55:28 -0400

    Finish updating package level test containers for new invocation spec
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- config/Dockerfiles/package_tests/ansible/ansible_package_test.sh
+++ config/Dockerfiles/package_tests/ansible/ansible_package_test.sh
@@ -2,7 +2,8 @@
 
 # This script requires that the docker run mounts the artifacts
 # dir to /tmp/artifacts, the ansible inventory file to /tmp/inventory,
-# and the ssh private key to /tmp/ssh_key
+# and the ssh private key to /tmp/ssh_key, so mount some dir with
+# the inventory and ssh_key to /tmp and expect artifacts there
 
 # Check if there is an upstream first repo for this package
 curl -s --head https://upstreamfirst.fedorainfracloud.org/${package} | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
@@ -10,15 +11,22 @@ if [ $? -ne 0 ]; then
      echo "No upstream repo for this package! Exiting..."
      exit 1
 fi
-# Clone standard-test-roles repo
-git clone https://pagure.io/standard-test-roles.git
-pushd standard-test-roles
 git clone https://upstreamfirst.fedorainfracloud.org/${package}
-if [ -f ${package}/test_local.yml ]; then
-     sed 's/hosts: localhost/hosts: all/' ${package}/test_local.yml > test_atomic.yml
+if [[ $(grep "standard-test-beakerlib" ${package}/*.yml) == "" ]]; then
+        echo "No beakerlib tests in this repo! Exiting.."
+        exit 0
+fi
+if [ -f ${package}/tests.yml ]; then
+     sed 's/hosts: localhost/hosts: all/g' ${package}/tests.yml > ${package}/test_atomic.yml
+     ansible-playbook -i /tmp/inventory --private-key=/tmp/ssh_key --tags=atomic --start-at-task='Define remote_artifacts if it is not already defined' ${package}/test_atomic.yml
+     exit $?
+# All code from here down is for legacy purposes and does not
+# guarantee the tests running are meant to pass on atomic host
+elif [ -f ${package}/test_local.yml ]; then
+     sed 's/hosts: localhost/hosts: all/g' ${package}/test_local.yml > ${package}/test_atomic.yml
 else
      # Write test_atomic.yml header
-     cat << EOF > test_atomic.yml
+     cat << EOF > ${package}/test_atomic.yml
 ---
 - hosts: all
   roles:
@@ -31,12 +39,8 @@ EOF
           exit 1
      fi
      for test in $(find ${package} -name "runtest.sh"); do
-          echo "    - $test" >> test_atomic.yml
+          echo "    - $test" >> ${package}/test_atomic.yml
      done
 fi
-# Get ready to execute tests
-sed -i 's|^artifacts\:.*|artifacts\: /tmp/artifacts|' roles/standard-test-beakerlib/vars/main.yml
-# Execute the tests
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i /tmp/inventory --private-key=/tmp/ssh_key --start-at-task='Define remote_artifacts if it is not already defined' test_atomic.yml
+ansible-playbook -i /tmp/inventory --private-key=/tmp/ssh_key --start-at-task='Define remote_artifacts if it is not already defined' ${package}/test_atomic.yml
 exit $?
--- config/Dockerfiles/package_tests/image/image_package_test.sh
+++ config/Dockerfiles/package_tests/image/image_package_test.sh
@@ -6,49 +6,59 @@ if [ $? -ne 0 ]; then
      echo "No upstream repo for this package! Exiting..."
      exit 1
 fi
-# Clone standard-test-roles repo
-git clone https://pagure.io/standard-test-roles.git
-pushd standard-test-roles
 git clone https://upstreamfirst.fedorainfracloud.org/${package}
-# Sym link ansible roles so they resolve properly
-mv /etc/ansible/roles /etc/ansible/roles-back
-ln -s $PWD/roles /etc/ansible/roles
-if ! [ -f ${package}/tests.yml ]; then
-# Write test_cloud.yml file
-cat << EOF > test_cloud.yml
----
-- hosts: localhost
-  vars:
-    artifacts: ./
-    playbooks: ./${package}/test_local.yml
-  vars_prompt:
-  - name: subjects
-    prompt: "A QCow2/raw test subject file"
-    private: no
-
-  roles:
-  - standard-test-cloud
-EOF
-     # Write test_local.yml header
-     cat << EOF > ${package}/test_local.yml
----
-- hosts: localhost
-  roles:
-  - role: standard-test-beakerlib
-    tests:
-EOF
-     # Find the tests
-     if [ $(find ${package} -name "runtest.sh" | wc -l) -eq 0 ]; then
-          echo "No runtest.sh files found in package's repo. Exiting..."
-          exit 1
-     fi
-     for test in $(find ${package} -name "runtest.sh"); do
-          echo "    - $test" >> ${package}/test_local.yml
-     done
-# Execute the tests legacy method
-sudo ansible-playbook test_cloud.yml -e artifacts=/tmp/test_output -e subjects=${image_location}
-exit $?
+if [[ $(grep "standard-test-beakerlib" ${package}/*.yml) == "" ]]; then
+	echo "No beakerlib tests in this repo! Exiting.."
+	exit 0
 fi
-# Execute the tests
-sudo ansible-playbook --tags=atomic ${package}/tests.yml -e TEST_SUBJECTS=${image_location} -e artifacts=/tmp/test_output
-exit $?
+if [[ $(file ${TEST_SUBJECTS}) == *"No such file or directory"* ]]; then
+	wget -q -O testimage.qcow2 ${TEST_SUBJECTS}
+	export TEST_SUBJECTS=${PWD}testimage.qcow2
+fi
+if [ -f ${package}/tests.yml ]; then
+     # Execute the tests
+     ansible-playbook --tags atomic ${package}/tests.yml
+     exit $?
+fi
+# Note: The below code should work, but we are not calling it.
+# The reason for this is that if repos do not have a tests.yml file,
+# then they have not been modified since test tagging came out,
+# which means we have no idea if the tests are meant to run/will pass
+# on atomic hosts.
+
+#else
+#     # Write test_cloud.yml file
+#     cat << EOF > test_cloud.yml
+#---
+#- hosts: localhost
+#  vars:
+#    artifacts: ./
+#    playbooks: ./${package}/test_local.yml
+#  vars_prompt:
+#  - name: subjects
+#    prompt: "A QCow2/raw test subject file"
+#    private: no
+#
+#  roles:
+#  - standard-test-cloud
+#EOF
+#     # Write test_local.yml header
+#     cat << EOF > ${package}/test_local.yml
+#---
+#- hosts: all
+#  roles:
+#  - role: standard-test-beakerlib
+#    tests:
+#EOF
+#     # Find the tests
+#     if [ $(find ${package} -name "runtest.sh" | wc -l) -eq 0 ]; then
+#          echo "No runtest.sh files found in package's repo. Exiting..."
+#          exit 1
+#     fi
+#     for test in $(find ${package} -name "runtest.sh"); do
+#          echo "    - $test" >> ${package}/test_local.yml
+#     done
+#     # Execute the tests legacy method
+#     ansible-playbook test_cloud.yml -e subjects=$TEST_SUBJECTS -e artifacts=$TEST_ARTIFACTS
+#     exit $?
+#fi
