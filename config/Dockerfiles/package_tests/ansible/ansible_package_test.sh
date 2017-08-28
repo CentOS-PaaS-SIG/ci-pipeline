3725e96b4fccc8beef946f2912635ed1aed6d0bb
Johnny Bieren <jbieren@redhat.com>
2017-08-28 11:48:28 -0400

    Dont fail if no repo exists
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- config/Dockerfiles/package_tests/ansible/ansible_package_test.sh
+++ config/Dockerfiles/package_tests/ansible/ansible_package_test.sh
@@ -9,7 +9,7 @@
 curl -s --head https://upstreamfirst.fedorainfracloud.org/${package} | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
 if [ $? -ne 0 ]; then
      echo "No upstream repo for this package! Exiting..."
-     exit 1
+     exit 0
 fi
 git clone https://upstreamfirst.fedorainfracloud.org/${package}
 if [[ $(grep "standard-test-beakerlib" ${package}/*.yml) == "" ]]; then
