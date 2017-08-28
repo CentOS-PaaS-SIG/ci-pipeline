9162ac29679bec2b000d5d8fc78264459b263ddc
Johnny Bieren <jbieren@redhat.com>
2017-08-28 12:00:45 -0400

    Wait for koji tasks to finish

--- config/Dockerfiles/rpmbuild/rpmbuild-test.sh
+++ config/Dockerfiles/rpmbuild/rpmbuild-test.sh
@@ -60,7 +60,7 @@ rpmbuild -bs /root/rpmbuild/SOURCES/${fed_repo}.spec
 kinit -k -t /home/fedora.keytab $FEDORA_PRINCIPAL
 # Build the package into ./results_${fed_repo}/$VERSION/$RELEASE/ and concurrently do a koji build
 { time fedpkg --release ${fed_branch} mockbuild ; } 2> ${LOGDIR}/mockbuild.txt &
-{ time koji build --scratch $RSYNC_BRANCH /root/rpmbuild/SRPMS/${fed_repo}*.src.rpm ; } 2> ${LOGDIR}/kojibuildtime.txt &
+{ time koji build --wait --scratch $RSYNC_BRANCH /root/rpmbuild/SRPMS/${fed_repo}*.src.rpm ; } 2> ${LOGDIR}/kojibuildtime.txt &
 # Set status if either job fails to build the rpm
 MOCKBUILD_STATUS=SUCCESS
 MOCKBUILD_RC=0
