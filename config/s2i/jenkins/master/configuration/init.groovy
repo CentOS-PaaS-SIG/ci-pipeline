23a051b43bd74334488e98faecc54e78cf734cfb
Scott Hebert <shebert@redhat.com>
2017-10-05 09:32:58 -0400

    Decorate our build with info at start of build

--- config/s2i/jenkins/master/configuration/init.groovy
+++ config/s2i/jenkins/master/configuration/init.groovy
@@ -11,14 +11,9 @@ logger.info("Enable Slave -> Master Access Control")
 Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
 Jenkins.instance.save()
 
-logger.info("Setting Time Zone to be EST")
-System.setProperty('org.apache.commons.jelly.tags.fmt.timeZone', 'America/New_York')
-
 logger.info("Setup fedora-fedmsg Messaging Provider")
 FedMsgMessagingProvider fedmsg = new FedMsgMessagingProvider("fedora-fedmsg", "tcp://hub.fedoraproject.org:9940", "tcp://hub.fedoraproject.org:9940", "org.fedoraproject");
-try {
-  GlobalCIConfiguration.get().addMessageProvider(fedmsg)
-} catch (Exception e) {
-  logger.info(e.getMessage())
-}
+GlobalCIConfiguration.get().addMessageProvider(fedmsg)
 
+logger.info("Setting Time Zone to be EST")
+System.setProperty('org.apache.commons.jelly.tags.fmt.timeZone', 'America/New_York')
