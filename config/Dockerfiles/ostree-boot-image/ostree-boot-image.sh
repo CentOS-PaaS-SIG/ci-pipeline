79686d10cd341af2a66600c4ac307a6a04ae6aea
Johnny Bieren <jbieren@redhat.com>
2017-10-19 08:20:32 -0400

    Try bpeck pr with new git username
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- config/Dockerfiles/ostree-boot-image/ostree-boot-image.sh
+++ config/Dockerfiles/ostree-boot-image/ostree-boot-image.sh
@@ -5,9 +5,11 @@ set -xeuo pipefail
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function clean_up {
-    #kill $(jobs -p)
-    #rm -rf $tmpdir
-    ara generate junit - > /root/output/logs/ansible_xunit.xml
+    set +e
+    ara generate junit - > /home/output/logs/ansible_xunit.xml
+    if [ -e "$base_dir/hosts" ]; then
+        ansible-playbook -i hosts ci-pipeline/playbooks/setup-libvirt-image.yml -e state=absent -e skip_init=true
+    fi
 }
 trap clean_up EXIT SIGHUP SIGINT SIGTERM
 
@@ -16,19 +18,17 @@ export ANSIBLE_CALLBACK_PLUGINS=$ara_location/plugins/callbacks
 export ANSIBLE_ACTION_PLUGINS=$ara_location/plugins/actions
 export ANSIBLE_LIBRARY=$ara_location/plugins/modules
 
-mkdir -p /root/output/logs
+mkdir -p /home/output/logs
 
-if [ -d "/root/output/images" ]; then
-    pushd /root/output
-    ls
-    ls images
+if [ -d "/home/output/images" ]; then
+    pushd /home/output
     if [ -d "images/latest-atomic.qcow2" ]; then
         # Use symlink if it exists
-        IMG_URL="/root/output/images/latest-atomic.qcow2"
+        IMG_URL="/home/output/images/latest-atomic.qcow2"
     else
         # Find the last image we pushed
         prev_img=$(ls -tr images/*.qcow2 | tail -n 1)
-        IMG_URL="/root/output/$prev_img"
+        IMG_URL="/home/output/$prev_img"
     fi
     popd
 fi
@@ -38,6 +38,7 @@ fi
 bootimage=${image2boot:-$IMG_URL}
 
 if ! [ -f ~/.ssh/id_rsa ]; then
+    mkdir -p ~/.ssh
     ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
     cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 fi
@@ -48,14 +49,10 @@ mkdir -p host_vars
 cat << 'EOF' > host_vars/localhost.yml
 qemu_img_path: /var/lib/libvirt/images
 bridge: virbr0
-virt_uri: qemu+tcp://libvirtd/system
 gateway: 192.168.122.1
 domain: local
 libvirt_systems:
  atomic-host-fedoraah:
-   ip: 192.168.122.200
-   remoteip: libvirtd
-   sshport: 2222
    admin_passwd: $5$uX5x24soDWv3G2TH$BYxhEq4HmxjKmyChV0.VTpqxfhqMaRk8LCr34KOg2C7
    memory: 3072
    disk: 10G
@@ -71,28 +68,32 @@ EOF
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-if [ "$state" = "absent" ]; then
-    ansible-playbook -i hosts ci-pipeline/playbooks/setup-libvirt-image.yml -e state=absent -e skip_init=true
-else
-    # Start test VM
-    ansible-playbook -i hosts ci-pipeline/playbooks/setup-libvirt-image.yml -e state=present -e skip_init=true
+# Start test VM
+ansible-playbook -i hosts ci-pipeline/playbooks/setup-libvirt-image.yml -e state=present -e skip_init=true
 
-    PROVISION_STATUS=$?
-    if [ "$PROVISION_STATUS" != 0 ]; then
-        echo "ERROR: Provisioning\nSTATUS: $PROVISION_STATUS"
-        exit 1
-    fi
+PROVISION_STATUS=$?
+if [ "$PROVISION_STATUS" != 0 ]; then
+    echo "ERROR: Provisioning\nSTATUS: $PROVISION_STATUS"
+    exit 1
+fi
 
-    cat << EOF > inventory
+ipaddress=$(cat libvirt-hosts | awk -F= '/ansible_ssh_host=/ { print $2 }')
+cat << EOF > inventory
 [ostree_compose_slave]
-libvirtd ansible_port=2222 ansible_user=admin ansible_ssh_pass=admin ansible_become=true ansible_become_pass=admin
+$ipaddress ansible_user=admin ansible_ssh_pass=admin ansible_become=true ansible_become_pass=admin
 EOF
 
-    ansible-playbook -i inventory ci-pipeline/playbooks/ostree-boot-verify.yml -l ostree_compose_slave -e "commit=$commit"
+ansible-playbook -i inventory ci-pipeline/playbooks/ostree-boot-verify.yml -l ostree_compose_slave -e "commit=$commit"
 
-    BOOT_STATUS=$?
-    if [ "$BOOT_STATUS" != 0 ]; then
-        echo "ERROR: Provisioning\nSTATUS: $BOOT_STATUS"
-        exit 1
-    fi
+BOOT_STATUS=$?
+if [ "$BOOT_STATUS" != 0 ]; then
+    echo "ERROR: Provisioning\nSTATUS: $BOOT_STATUS"
+    exit 1
+fi
+
+# If image2boot is defined then symlink it as latest
+if [ "${image2boot:-unset}" != "unset" ]; then
+    pushd /home/output/images
+    sudo ln -sf $(basename $image2boot) latest-atomic.qcow2
+    popd
 fi
