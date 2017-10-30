d6306dbfce996c866f3b6aee6cc6091afb6ccec8
Johnny Bieren <jbieren@redhat.com>
2017-10-30 08:37:38 -0400

    foo
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- config/Dockerfiles/rsync/rsync_no_path.sh
+++ config/Dockerfiles/rsync/rsync_no_path.sh
@@ -9,10 +9,10 @@ if [[ ! -v rsync_opts ]]; then
     rsync_opts="--delete"
 fi
 
-rsync -q ${rsync_from}/
+rsync -q ${rsync_from}
 # rsync rc:23     Partial transfer due to error
 if [ $? -eq 23 ]; then
     echo "Partial transfer due to error, could be source doesn't exist?"
 else
-    rsync ${rsync_opts} --stats -a ${rsync_from}/ ${rsync_to}/
+    rsync ${rsync_opts} --stats -a ${rsync_from} ${rsync_to}
 fi
