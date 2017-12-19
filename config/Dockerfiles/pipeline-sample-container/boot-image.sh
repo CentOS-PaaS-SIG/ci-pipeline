10f51052b08b25314918063c233331205d54c38d
Ari LiVigni <alivigni@redhat.com>
2017-12-19 00:40:24 -0500

    Add pipeline-sample for an example project
    - Add Dockerfile and scripts

--- config/Dockerfiles/pipeline-sample-container/boot-image.sh
+++ config/Dockerfiles/pipeline-sample-container/boot-image.sh
@@ -12,7 +12,14 @@ function clean_up {
         ansible-playbook -i hosts ${base_dir}/ci-pipeline/playbooks/setup-libvirt-image.yml -e state=absent -e skip_init=true
     fi
 }
-#trap clean_up EXIT SIGHUP SIGINT SIGTERM
+trap clean_up EXIT SIGHUP SIGINT SIGTERM
+
+
+curl -o /var/lib/libvirt/images/latest-atomic.qcow2 -z /var/lib/libvirt/images/latest-atomic.qcow2 ${IMG_URL}
+if [ -f ]; then
+    mkdir ${base_dir}/images
+    ln -fs /var/lib/libvirt/images/latest-atomic.qcow2 ${base_dir}/images/latest-atomic.qcow2
+fi
 
 export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
 export ANSIBLE_CALLBACK_PLUGINS=$ara_location/plugins/callbacks
@@ -21,18 +28,18 @@ export ANSIBLE_LIBRARY=$ara_location/plugins/modules
 
 mkdir -p ${base_dir}/logs
 
-if [ -d "${base_dir}/images" ]; then
-    pushd ${base_dir}
-    if [ -d "images/latest-atomic.qcow2" ]; then
-        # Use symlink if it exists
-        IMG_URL="${base_dir}/images/latest-atomic.qcow2"
-    else
-        # Find the last image we pushed
-        prev_img=$(ls -tr images/*.qcow2 | tail -n 1)
-        IMG_URL="${base_dir}/$prev_img"
-    fi
-    popd
-fi
+#if [ -d "${base_dir}/images" ]; then
+#    pushd ${base_dir}
+#    if [ -d "images/latest-atomic.qcow2" ]; then
+#        # Use symlink if it exists
+#        IMG_URL="${base_dir}/images/latest-atomic.qcow2"
+#    else
+#        # Find the last image we pushed
+#        prev_img=$(ls -tr images/*.qcow2 | tail -n 1)
+#        IMG_URL="${base_dir}/$prev_img"
+#    fi
+#    popd
+#fi
 
 # If image2boot is defined use that image, but if not fall back to the
 # previous image built
@@ -70,20 +77,14 @@ EOF
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 # Start test VM
-#ansible-playbook -i hosts ${base_dir}/ci-pipeline/playbooks/setup-libvirt-image.yml -e state=present -e skip_init=true
+ansible-playbook -i hosts ${base_dir}/ci-pipeline/playbooks/setup-libvirt-image.yml -e state=present -e skip_init=true
 
-#ipaddress=$(cat libvirt-hosts | awk -F= '/ansible_ssh_host=/ { print $2 }')
-#cat << EOF > inventory
-#[pipeline_sample_container_slave]
-#$ipaddress ansible_user=admin ansible_ssh_pass=admin ansible_become=true ansible_become_pass=admin
-#EOF
+ipaddress=$(cat libvirt-hosts | awk -F= '/ansible_ssh_host=/ { print $2 }')
+cat << EOF > inventory
+[pipeline_sample_container_slave]
+$ipaddress ansible_user=admin ansible_ssh_pass=admin ansible_become=true ansible_become_pass=admin
+EOF
 
 #ansible-playbook -i inventory ${base_dir}/ci-pipeline/playbooks/pipeline-sample.yml -l pipeline_sample_container_ -e "commit=$commit"
-ansible-playbook -i inventory ${base_dir}/ci-pipeline/playbooks/pipeline-sample.yml
+ansible-playbook -i inventory ${base_dir}/ci-pipeline/playbooks/pipeline-sample.yml -l pipeline_sample_container
 
-# If image2boot is defined then symlink it as latest
-#if [ "${image2boot:-unset}" != "unset" ]; then
-#    pushd ${base_dir}/images
-#    ln -sf $(basename $image2boot) latest-atomic.qcow2
-#    popd
-#fi
