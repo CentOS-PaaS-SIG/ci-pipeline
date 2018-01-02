0fe7f85a4ce3b757a6d116a48ee70e148e7553d2
Ari LiVigni <alivigni@redhat.com>
2018-01-02 03:18:09 -0500

    Add pipeline-sample for an example project
    - Add Dockerfile and scripts

--- config/Dockerfiles/pipeline-sample-container/execute.sh
+++ config/Dockerfiles/pipeline-sample-container/execute.sh
@@ -45,18 +45,31 @@ if [ "${ENABLE_VM}" == "true" ]; then
     pubkey=$(cat ~/.ssh/id_rsa.pub)
 
     mkdir -p host_vars
-    printf "qemu_img_path: /home/images\nbridge: virbr1\ngateway: 192.168.123.1\ndomain: local\nlibvirt_systems:\n atomic-host-fedoraah:\n" > host_vars/localhost.yml
-    printf "  admin_passwd: \$5\$uX5x24soDWv3G2TH\$BYxhEq4HmxjKmyChV0.VTpqxfhqMaRk8LCr34KOg2C7\n   memory: 3072\n   disk: 10G\n" >> host_vars/localhost.yml
-    printf "   img_url: ${IMG_URL}\n   admin_ssh_rsa: $pubkey\n" >> host_vars/localhost.yml
+    cat << 'EOF' > host_vars/localhost.yml
+    qemu_img_path: /var/lib/libvirt/images
+    bridge: virbr1
+    gateway: 192.168.123.1
+    domain: local
+    libvirt_systems:
+     atomic-host-fedoraah:
+       admin_passwd: $5$uX5x24soDWv3G2TH$BYxhEq4HmxjKmyChV0.VTpqxfhqMaRk8LCr34KOg2C7
+       memory: 3072
+       disk: 10G
+EOF
+    cat << EOF >> host_vars/localhost.yml
+       img_url: $bootimage
+       admin_ssh_rsa: $pubkey
+EOF
 
     # Start test VM
     ansible-playbook -vv -i hosts ${base_dir}/ci-pipeline/playbooks/setup-libvirt-image.yml -e state=present -e skip_init=true
 
-    ipaddress=$(cat libvirt-hosts | awk -F= '/ansible_ssh_host=/ { print $2 }')
-
     # For running playbook on a cloud image VM inside the sample pipeline container
-    printf "[pipeline_sample_container_slave]\n$ipaddress ansible_user=admin ansible_ssh_pass=admin ansible_become=true ansible_become_pass=admin\n" > inventory
-
+    ipaddress=$(cat libvirt-hosts | awk -F= '/ansible_ssh_host=/ { print $2 }')
+    cat << EOF > inventory
+    [pipeline_sample_container_slave]
+    $ipaddress ansible_user=admin ansible_ssh_pass=admin ansible_become=true ansible_become_pass=admin
+EOF
     echo "Running example playbook on a cloud VM image inside the sample pipeline container"
     ansible-playbook -vv i inventory ${base_dir}/ci-pipeline/playbooks/pipeline-sample-boot-verify.yml -l pipeline_sample_container_slave
 else
