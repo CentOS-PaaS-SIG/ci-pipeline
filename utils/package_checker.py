dadcbdea41656b515ccc3e65df85ed8c656a5d43
Johnny Bieren <jbieren@redhat.com>
2018-01-04 10:51:20 -0500

    Good feedback
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- utils/package_checker.py
+++ utils/package_checker.py
@@ -1,27 +1,25 @@
-#!/bin/env python
+# !/bin/env python
 import json
 import sys
-import os.path
 
-# Exit 2 if upstream package list doesnt exist
-# Exit 1 if file exists, but package isnt in the list
+# Exit 2 if upstream package list doesn't exist
+# Exit 1 if file exists, but package isn't in the list
+# Exit 0 if file exists, and package is in the list
 
 jsonpath = 'fedora-atomic/fedora-atomic-host-base.json'
 
-# Make sure json file exists
-if not os.path.isfile(jsonpath):
+try:
+    with open(jsonpath, 'r') as f:
+        atomicjson = json.load(f)
+    mypackage = sys.argv[1]
+
+    # Check if package exists in the json file
+    # Check both all packages and x86_64 specific packages
+    if mypackage in atomicjson["packages"] or mypackage in atomicjson["packages-x86_64"]:
+        print ("Package of interest!")
+        sys.exit(0)
+    # Fail if not
+    sys.exit(1)
+except IOError as e:
     print("Could not find upstream package json file")
     sys.exit(2)
-
-jsonfile = open(jsonpath, 'r').read()
-atomicjson = json.loads(jsonfile)
-mypackage = sys.argv[1]
-
-# Check if package exists in the json file
-# Check both all packages and x86_64 specific packages
-if mypackage in atomicjson["packages"] or mypackage in atomicjson["packages-x86_64"]:
-    print ("Package of interest!")
-    sys.exit(0)
-
-# Fail if not
-sys.exit(1)
