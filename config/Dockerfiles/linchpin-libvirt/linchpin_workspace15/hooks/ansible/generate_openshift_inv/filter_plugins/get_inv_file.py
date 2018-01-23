5cfbd179d45e71c352134acf8597764ce37c558f
samvarankashyap <samvaran.kashyap@gmail.com>
2018-01-23 14:41:00 -0500

    Updated inventory_file fetch according to linchpin 1.5

--- config/Dockerfiles/linchpin-libvirt/linchpin_workspace15/hooks/ansible/generate_openshift_inv/filter_plugins/get_inv_file.py
+++ config/Dockerfiles/linchpin-libvirt/linchpin_workspace15/hooks/ansible/generate_openshift_inv/filter_plugins/get_inv_file.py
@@ -7,7 +7,7 @@ def get_inv_file(filepath):
     list_of_files = glob.glob(filepath+'*')
     print list_of_files
     latest_file = max(list_of_files, key=os.path.getctime)
-    return filepath+latest_file
+    return latest_file
 
 class FilterModule(object):
     ''' A filter to fetch latest file '''
