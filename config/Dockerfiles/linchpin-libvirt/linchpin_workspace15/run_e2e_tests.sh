b314293afd5a38ada19e9766785d3e0256487ec2
samvarankashyap <samvaran.kashyap@gmail.com>
2018-01-24 16:55:17 -0500

    added retry logic to linchpin up

--- config/Dockerfiles/linchpin-libvirt/linchpin_workspace15/run_e2e_tests.sh
+++ config/Dockerfiles/linchpin-libvirt/linchpin_workspace15/run_e2e_tests.sh
@@ -4,10 +4,19 @@ su - -c "echo Running as Username $USER"
 echo "Inserting img_src"
 img_src=$(find /workDir/workspace -name "untested-atomic.qcow2" | tail -n 1)
 sed -i "s|        image_src\:.*|        image_src\: \"file\:\/\/$img_src\"|" /root/linchpin_workspace/topologies/example-topology.yml
-
-echo "Running linchpin up inside libvirt container"
-# run command as root
 su - -c "mkdir ~/.ssh"
-su - -c "linchpin -v -w /root/linchpin_workspace up"
+
+retries=3
+
+for ((i=0; i<retries; i++)); do
+    echo "Running linchpin up inside libvirt container"
+    su - -c "linchpin -v -w /root/linchpin_workspace up" && su - -c "linchpin -v -w /root/linchpin_workspace drop" 
+    [[ $? -eq 0 ]] && break
+
+    echo "something went wrong, let's wait 20 seconds and retry"
+    sleep 20
+done
+
+[[ $retries -eq i ]] && { echo "Failed!"; exit 1; }
 
 cat /tmp/e2e.log
