a089142d4df5338cd1cd91eae380c3f04b7257a0
samvarankashyap <samvaran.kashyap@gmail.com>
2018-01-24 11:50:13 -0500

    Updated machine specifications for a faster boot

--- config/Dockerfiles/linchpin-libvirt/linchpin_workspace15/run_e2e_tests.sh
+++ config/Dockerfiles/linchpin-libvirt/linchpin_workspace15/run_e2e_tests.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -x
 
+su - -c "echo Running as Username $USER"
 echo "Inserting img_src"
 img_src=$(find /workDir/workspace -name "untested-atomic.qcow2" | tail -n 1)
 sed -i "s|        image_src\:.*|        image_src\: \"file\:\/\/$img_src\"|" /root/linchpin_workspace/topologies/example-topology.yml
 
 echo "Running linchpin up inside libvirt container"
-
-linchpin -v -w /root/linchpin_workspace up
+# run command as root
+su - -c "mkdir ~/.ssh"
+su - -c "linchpin -v -w /root/linchpin_workspace up"
 
 cat /tmp/e2e.log
