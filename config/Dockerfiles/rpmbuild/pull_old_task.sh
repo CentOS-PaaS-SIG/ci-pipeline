4ff53fc4f66a74df7d0b894c97dd3c1ebdde517f
Johnny Bieren <jbieren@redhat.com>
2018-03-23 08:55:48 -0400

    Add branch to cloud image compose variables

--- config/Dockerfiles/rpmbuild/pull_old_task.sh
+++ config/Dockerfiles/rpmbuild/pull_old_task.sh
@@ -25,7 +25,8 @@ function archive_variables {
     cat << EOF > ${LOGDIR}/job.props
 koji_task_id=${PROVIDED_KOJI_TASKID}
 fed_repo=${PACKAGE}
-fed_branch=${BRANCH}
+fed_branch=${FED_BRANCH}
+branch=${BRANCH}
 fed_rev=kojitask${PROVIDED_KOJI_TASKID}
 nvr=${NVR}
 original_spec_nvr=${NVR}
@@ -39,8 +40,15 @@ koji download-task ${PROVIDED_KOJI_TASKID} --logs
 createrepo .
 PACKAGE=$(rpm --queryformat "%{NAME}\n" -qp *.src.rpm)
 NVR=$(rpm --queryformat "%{NAME} %{VERSION} %{RELEASE}\n" -qp *.src.rpm)
-BRANCH=$(grep -Po "chrootPath='/var/lib/mock/\K[^-]+" build.*.log | head -n 1)
+FED_BRANCH=$(grep -Po "chrootPath='/var/lib/mock/\K[^-]+" build.*.log | head -n 1)
 popd
+
+if [ "${FED_BRANCH}" = $(curl -s https://src.fedoraproject.org/rpms/fedora-release/raw/master/f/fedora-release.spec | awk '/%define dist_version/ {print $3}') ]; then
+    BRANCH="master"
+    FED_BRANCH="rawhide"
+else
+    BRANCH=$FED_BRANCH
+fi
 
 # Let's archive the logs too
 cp ${RPMDIR}/*.log ${LOGDIR}/
