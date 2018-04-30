8d30ed15669074b4d818e94fffdf295aab24494a
Joe Stuart <jstuart@redhat.com>
2018-04-30 15:02:30 -0500

    fixes for fedora pipeline

--- config/Dockerfiles/cloud-image-compose/virt-customize.sh
+++ config/Dockerfiles/cloud-image-compose/virt-customize.sh
@@ -2,13 +2,12 @@
 
 set -xeuo pipefail
 
-# A shell script that pulls the latest nightly from rcm
+# A shell script that pulls the latest Fedora cloud build
 # and uses virt-customize to inject rpms into it. It 
 # outputs a new qcow2 image for you to use.
-#
-# Note: This is only configured for rhel8 currently
 
 CURRENTDIR=$(pwd)
+
 if [ ${CURRENTDIR} == "/" ] ; then
     cd /home
     CURRENTDIR=/home
@@ -23,13 +22,13 @@ virtlogd &
 
 chmod 666 /dev/kvm
 
-if [$branch != "rawhide"]; then
+if [ $branch != "rawhide" ]; then
     branch=${branch:1}
 fi
 
 # Define proper install url
 if [[ $(curl -q https://dl.fedoraproject.org/pub/fedora/linux/development/ | grep "${branch}/") != "" ]]; then
-    INSTALL_URL="https://dl.fedoraproject.org/pub/fedora/linux/development/${branch}/CloudImages/x86_64/images/"
+    INSTALL_URL="https://dl.fedoraproject.org/pub/fedora/linux/development/${branch}/Cloud/x86_64/images/"
 elif [[ $(curl -q https://dl.fedoraproject.org/pub/fedora/linux/releases/ | grep "${branch}/") != "" ]]; then
     INSTALL_URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/${branch}/CloudImages/x86_64/images/"
 else
@@ -47,10 +46,6 @@ function clean_up {
   ln -sf $(find . -name "*.qcow2") test_subject.qcow2
   popd
   kill $(jobs -p)
-  # In case of first build, ensure image_artifacts dir exists
-  mkdir -p ${image_artifacts}
-  cp -rp ${CURRENTDIR}/images/* ${CURRENTDIR}/logs ${image_artifacts}
-  #ln -sf ${ARTIFACT_MNT}/${package}  ${ARTIFACT_MNT}/latest
 }
 trap clean_up EXIT SIGHUP SIGINT SIGTERM
 
@@ -58,22 +53,16 @@ trap clean_up EXIT SIGHUP SIGINT SIGTERM
 
 mkdir -p ${CURRENTDIR}/images
 
+RPM_LIST=""
+REPO_LIST="--repofrompath=${package},file:///etc/yum.repos.d"
 # Add custom rpms to image
-while read rpm ; do
-    rpm_name=$(echo $rpm | cut -d ' ' -f 1)
-    TIMEDIFF=$(expr $(date '+%s') - $(date '+%s' -d $(echo $rpm | cut -d ' ' -f 2)))
-    # Only rpms saved within last day
-    if [[ $TIMEDIFF -le 86400 ]]; then
-        virt-copy-in -a ${DOWNLOADED_IMAGE_LOCATION} ${rpm_repo}/${rpm_name} /etc/yum.repos.d/
-        for pkg in $(repoquery --disablerepo=\* --enablerepo=${rpm_name} --repofrompath=${rpm_name},${rpm_repo} --all | grep -v '\-debug\|\-devel' | rev | cut -d '-' -f 3- | rev ) ; do
-            if ! virt-customize -a ${DOWNLOADED_IMAGE_LOCATION} --run-command "yum install -y --nogpgcheck --repofrompath=testrepo,file:///etc/yum.repos.d/${rpm_name} ${pkg}" ; then
-                if [ $package == $rpm_name ] ; then
-                    sed -i "/^${package}/d" ${manifest}
-                fi
-                exit 1
-            fi
-        done
-    fi
-done < $manifest
+virt-copy-in -a ${DOWNLOADED_IMAGE_LOCATION} ${rpm_repo}/*.rpm ${rpm_repo}/repodata /etc/yum.repos.d/
+
+for pkg in $(dnf repoquery --disablerepo=\* --enablerepo=${package} --repofrompath=${package},${rpm_repo} --all | egrep -v '\-debug\|\-devel|.src' | rev | cut -d '-' -f 3- | rev ) ; do
+    RPM_LIST="${RPM_LIST} ${pkg}"
+done
+if ! virt-customize -a ${DOWNLOADED_IMAGE_LOCATION} --run-command "dnf install -y --nogpgcheck ${REPO_LIST} ${RPM_LIST}" ; then
+    echo "failure"
+fi
 
 } 2>&1 | tee ${CURRENTDIR}/logs/console.log #group for tee
