a302418cf1ee13fed549d35b1f6e660bcbc9db76
Johnny Bieren <jbieren@redhat.com>
2018-05-08 08:45:39 -0400

    Quote groovy vars

--- src/org/centos/pipeline/PipelineUtils.groovy
+++ src/org/centos/pipeline/PipelineUtils.groovy
@@ -1218,18 +1218,18 @@ def obtainLock(String fileLocation, int duration) {
         currentTime=$(date +%s)
         while true ; do
             # Check if lock file exists
-            while [ -f ${fileLocation} ] ; do
-                lockAge=$(stat -c %Y ${fileLocation})
+            while [ -f "${fileLocation}" ] ; do
+                lockAge=$(stat -c %Y "${fileLocation}")
                 ageDiff=$((${currentTime} - ${lockAge}))
                 # Break if lock file is too old
-                if [ ${ageDiff} -ge ${duration} ]; then
+                if [ ${ageDiff} -ge "${duration}" ]; then
                     break
                 fi
             done
             # Now, either lock file is older than duration
             # or the lock is gone, so proceed
-            echo ${myuuid} > ${fileLocation}
-            testuuid=$(cat ${fileLocation})
+            echo ${myuuid} > "${fileLocation}"
+            testuuid=$(cat "${fileLocation}")
             # If uuid matches, we got the lock
             if [ ${testuuid} == ${myuuid} ]; then
                 break
@@ -1237,7 +1237,7 @@ def obtainLock(String fileLocation, int duration) {
             sleep 30
         done
         # fileLocation.lck isn't important, but redirect somewhere
-        ) 9>${fileLocation}.lck
+        ) 9>"${fileLocation}".lck
     """
     return myuuid
 }
