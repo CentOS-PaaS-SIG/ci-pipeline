bf3621672d9b51ca1dbb2b22c14096a2e1921ff9
Johnny Bieren <jbieren@redhat.com>
2018-06-06 14:35:14 -0400

    Switch to using mdapi
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- utils/package_checker.py
+++ utils/package_checker.py
@@ -21,38 +21,30 @@ try:
         sys.exit(0)
     # Check if a package that comes from this distgit repo is in atomic
     else:
-        # Open up a connection to the PDC
-        pdc_server = httplib.HTTPSConnection('pdc.fedoraproject.org', timeout=10)
-        # Get package name from args
+        # Open up a connection to mdapi
+        mdapi_server = httplib.HTTPSConnection('apps.fedoraproject.org', timeout=10)
+        # Get package name and branch from args
         mypackage = sys.argv[1]
+        mybranch = sys.argv[2]
         
-        # Set up array of packages
-        childPackages = []
-        # Set first page to pull. We only need the name field, which speeds it up
-        resultPage = "/rest_api/v1/rpms/?arch=x86_64&arch=noarch&fields=name&srpm_name=" + mypackage
-        while resultPage != None:
-            pdc_server.request("GET",resultPage)
-            res = pdc_server.getresponse()
-            if res.status != 200:
-                print("PDC lookup failed for %s" % resultPage)
-                sys.exit(2)
-            pdc_message = res.read()
-            # Convert to json
-            pdc_parsed = json.loads(pdc_message)
-            if 'results' in pdc_parsed:
-                # Add packages to array
-                for package in pdc_parsed['results']:
-                    childPackages = childPackages + [package['name']]
-            # This will be None on the last page
-            resultPage = pdc_parsed['next']
-    # Perform the check
-    if set(atomicjson["packages"]).isdisjoint(childPackages) and set(atomicjson["packages-x86_64"]).isdisjoint(childPackages):
-        # Sets are disjoint so package is not in atomic host
-        sys.exit(1)
-    else:
-        # Sets are not disjoint so package is in atomic host
-        print ("Package of interest!")
-        sys.exit(0)
+        resultPage = "/mdapi/" + mybranch + "/pkg/" + mypackage
+        mdapi_server.request("GET",resultPage)
+        res = mdapi_server.getresponse()
+        if res.status != 200:
+            print("mdapi lookup failed for %s" % resultPage)
+            sys.exit(2)
+        mdapi_message = res.read()
+        # Convert to json
+        mdapi_parsed = json.loads(mdapi_message)
+        if "co-packages" in mdapi_parsed:
+            # Perform the check
+            if set(atomicjson["packages"]).isdisjoint(mdapi_parsed["co-packages"]) and set(atomicjson["packages-x86_64"]).isdisjoint(mdapi_parsed["co-packages"]):
+                # Sets are disjoint so package is not in atomic host
+                sys.exit(1)
+            else:
+                # Sets are not disjoint so package is in atomic host
+                print ("Package of interest!")
+                sys.exit(0)
 except IOError as e:
     print("Could not find upstream package json file")
     sys.exit(2)
