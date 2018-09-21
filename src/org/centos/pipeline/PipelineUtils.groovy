52b0925b75f57d64a973050860fa716a1353bf59
Clint Savage <herlo@redhat.com>
2018-09-21 10:57:31 -0600

    more debug

--- src/org/centos/pipeline/PipelineUtils.groovy
+++ src/org/centos/pipeline/PipelineUtils.groovy
@@ -955,7 +955,7 @@ def setupContainerTemplates(String openshiftProject) {
     openshift.withCluster() {
         openshift.withProject(openshiftProject) {
             dir('config/s2i') {
-                sh './create-containers.sh'
+                sh 'bash -x ./create-containers.sh'
             }
         }
     }
