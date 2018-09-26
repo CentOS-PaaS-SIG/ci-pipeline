711efcd85a2feb6b4a2f0d0c418f011c04a87464
Johnny Bieren <jbieren@redhat.com>
2018-09-26 13:46:17 -0400

    Add systemd changes to container
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- config/Dockerfiles/str-container-test/container-test.sh
+++ config/Dockerfiles/str-container-test/container-test.sh
@@ -93,7 +93,6 @@ set -u
 set -xo pipefail
 for playbook in tests*.yml; do
 	if [ -f ${playbook} ]; then
-		# TODO: nvr needs to be standardized later in STR
 		timeout 4h ansible-playbook -v --inventory=$ANSIBLE_INVENTORY $PYTHON_INTERPRETER \
 			--extra-vars "artifacts=$TEST_ARTIFACTS" \
 			--tags ${TAG} ${playbook} $@ | tee ${TEST_ARTIFACTS}/${playbook}-run.txt
